78d2fe848540ba0c16bf842896279a1f8b65c275
Rustam <hrustbas@gmail.com>
2024-04-21 09:30:09 +0300

    add trapecia method

--- integrate/test.c
+++ integrate/test.c
@@ -9,3 +9,15 @@ float squares(float a, float b, unsigned N, float (*f)(float)) {
 
     return result;
 }
+
+float trapecia(float a, float b, unsigned N, float (*f)(float)) {
+    float h = (b-a)/N;
+    float result = 0.0;
+
+    while (a < b) {
+        result += h * (f(a)+f(a+h))/2;
+        a += h;
+    }
+
+    return result;
+}
