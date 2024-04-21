433fc90b52097bdabec2eb7377993176e668697c
Rustam <hrustbas@gmail.com>
2024-04-21 09:24:37 +0300

    simple integrator

--- integrate/test.c
+++ integrate/test.c
@@ -1,3 +1,11 @@
-float squares(float a, float b, float (*f)) {
-    return 0.0;
+float squares(float a, float b, unsigned N, float (*f)(float)) {
+    float h = (b-a)/N;
+    float result = 0.0;
+
+    while (a < b) {
+        result += h * f(a+h/2);
+        a += h;
+    }
+
+    return result;
 }
--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include "integrate/test.c"
 #include <stdio.h>
+#include <math.h>
+
+float f(float x) {
+    return x*x;
+}
 
 int main() {
-    printf("test\n");
+    printf("n,error\n");
+    for (int i=1; i<=1000; i++) {
+        printf("%2d,%6f\n", i, fabs(squares(0,5,i,f)-125.0/3));
+    }
 }
