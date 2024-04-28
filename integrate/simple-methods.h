497fd5ad0dd155958e1bd35d4cb4bce8664fec9d
Rustam <hrustbas@gmail.com>
2024-04-28 13:08:58 +0300

    create inclusion guards

--- integrate/simple-methods.h
+++ integrate/simple-methods.h
@@ -1,23 +1,28 @@
+#ifndef MATH_H
+#define MATH_H
+#include <math.h>
+#endif
+
 float squares(float a, float b, unsigned N, float (*f)(float)) {
-    float h = (b-a)/N;
-    float result = 0.0;
+  float h = (b - a) / N;
+  float result = 0.0;
 
-    while (a < b) {
-        result += h * f(a+h/2);
-        a += h;
-    }
+  while (a < b) {
+    result += h * f(a + h / 2);
+    a += h;
+  }
 
-    return result;
+  return result;
 }
 
 float trapecia(float a, float b, unsigned N, float (*f)(float)) {
-    float h = (b-a)/N;
-    float result = 0.0;
+  float h = (b - a) / N;
+  float result = 0.0;
 
-    while (a < b) {
-        result += h * (f(a)+f(a+h))/2;
-        a += h;
-    }
+  while (a < b) {
+    result += h * (f(a) + f(a + h)) / 2;
+    a += h;
+  }
 
-    return result;
+  return result;
 }
--- solvers/simple.h
+++ solvers/simple.h
@@ -1,4 +1,8 @@
-// TODO: include all libs with inclusions guards
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+
 float **solve(unsigned M, unsigned N, float *t,
               float *(*dxdt)(float *, float, unsigned), float *X0) {
   float **result = (float **)malloc(sizeof(float *) * N);
