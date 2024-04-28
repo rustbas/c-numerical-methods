0fc8a118303380add253834d583a666eeeb7b2e8
Rustam <hrustbas@gmail.com>
2024-04-28 13:02:42 +0300

    prettify

--- solvers/simple.h
+++ solvers/simple.h
@@ -1,35 +1,30 @@
-float** solve(unsigned M, 
-        unsigned N, 
-        float* t, 
-        void (*dxdt)(float*, float, unsigned, float*),
-        float* X0) 
-{
-    float** result = (float**) malloc(sizeof(float*) * N);
-    for (int i=0; i<N; i++) {
-        result[i] = (float*) malloc(sizeof(float)*M);
+// TODO: include all libs with inclusions guards
+float **solve(unsigned M, unsigned N, float *t,
+              float *(*dxdt)(float *, float, unsigned), float *X0) {
+  float **result = (float **)malloc(sizeof(float *) * N);
+  for (int i = 0; i < N; i++) {
+    result[i] = (float *)malloc(sizeof(float) * M);
+  }
+
+  float *tmp = (float *)malloc(sizeof(float) * M);
+  for (int i = 0; i < M; i++) {
+    tmp[i] = X0[i];
+    result[0][i] = tmp[i];
+  }
+
+  float *tmpd = (float *)malloc(sizeof(float) * M);
+
+  for (int i = 1; i < N; i++) {
+
+    tmpd = dxdt(result[i - 1], t[i], M);
+
+    for (int j = 0; j < M; j++) {
+      result[i][j] = result[i - 1][j] + tmpd[j] * (t[i] - t[i - 1]);
     }
+  }
 
-    float* tmp = (float*) malloc(sizeof(float) * M);
-    for (int i=0; i<M; i++) {
-        tmp[i] = X0[i];
-        result[0][i] = tmp[i];
-    }
+  free(tmp);
+  free(tmpd);
 
-    float* tmpd = (float*) malloc(sizeof(float) * M);
-
-    for (int i=1; i<N; i++) {
-
-        dxdt(result[i-1], t[i], M, tmpd);
-
-        for (int j=0; j<M; j++) {
-            result[i][j] = result[i-1][j] + tmpd[j] * (t[i] - t[i-1]);
-        }
-
-
-    }
-
-    free(tmp);
-    free(tmpd);
-
-    return result;
+  return result;
 }
