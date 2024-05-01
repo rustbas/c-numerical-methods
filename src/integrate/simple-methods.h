eb903e255605c0fef9958664cf8c7a5b89141070
Rustam <hrustbas@gmail.com>
2024-05-01 15:55:52 +0300

    removed c-files

--- src/integrate/simple-methods.h
+++ src/integrate/simple-methods.h
@@ -1,3 +1,6 @@
+#ifndef _INTEGRATE_SIMPLE_METHODS
+#define _INTEGRATE_SIMPLE_METHODS
+
 #ifndef MATH_H
 #define MATH_H
 #include <math.h>
@@ -5,3 +8,35 @@
 
 float squares(float a, float b, unsigned N, float (*f)(float));
 float trapecia(float a, float b, unsigned N, float (*f)(float));
+#endif
+
+// ----------------------------------------
+// ----------------------------------------
+// ----------------------------------------
+
+#ifdef INTEGRATE_SM_IMPLEMENTATION
+float squares(float a, float b, unsigned N, float (*f)(float)) {
+  float h = (b - a) / N;
+  float result = 0.0;
+
+  while (a < b) {
+    result += h * f(a + h / 2);
+    a += h;
+  }
+
+  return result;
+}
+
+float trapecia(float a, float b, unsigned N, float (*f)(float)) {
+  float h = (b - a) / N;
+  float result = 0.0;
+
+  while (a < b) {
+    result += h * (f(a) + f(a + h)) / 2;
+    a += h;
+  }
+
+  return result;
+}
+
+#endif
