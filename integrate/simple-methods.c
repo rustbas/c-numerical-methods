d1b8d4e062caffbd0f3a2d9d55299af2c16d8fa1
Basyrov Rustam <bronit10@morozko.ru>
2024-05-19 11:40:24 +0300

    add double version

--- integrate/simple-methods.c
+++ integrate/simple-methods.c
@@ -19,6 +19,9 @@
 
 float squares(float a, float b, unsigned N, float (*f)(float));
 float trapecia(float a, float b, unsigned N, float (*f)(float));
+
+double squaresd(double a, double b, unsigned N, double (*f)(double));
+double trapeciad(double a, double b, unsigned N, double (*f)(double));
 #endif
 
 // ----------------------------------------
@@ -53,6 +56,44 @@ float trapecia(float a, float b, unsigned N, float (*f)(float)) {
   float result = 0.0;
 
   float *xs = (float *)malloc(sizeof(float) * N);
+  int i;
+  for (i = 0; i < N; i++) {
+    xs[i] = a + h * i;
+  }
+
+  for (i = 0; i < N - 1; i++) {
+    // result += (xs[i] - xs[i - 1]) * f((xs[i] + xs[i - 1]) / 2);
+    result += h * (f(xs[i]) + f(xs[i + 1])) / 2.0f;
+  }
+
+  free(xs);
+  return result;
+}
+
+double squaresd(double a, double b, unsigned N, double (*f)(double)) {
+  double h = (b - a) / (N - 1);
+  double result = 0.0;
+  double *xs = (double *)malloc(sizeof(double) * N);
+  int i;
+  for (i = 0; i < N; i++) {
+    xs[i] = a + h * i;
+  }
+
+  for (i = 0; i < N - 1; i++) {
+    // result += (xs[i] - xs[i - 1]) * f((xs[i] + xs[i - 1]) / 2);
+    result += h * f((xs[i] + xs[i + 1]) / 2);
+  }
+
+  free(xs);
+
+  return result;
+}
+
+double trapeciad(double a, double b, unsigned N, double (*f)(double)) {
+  double h = (b - a) / (N - 1);
+  double result = 0.0;
+
+  double *xs = (double *)malloc(sizeof(double) * N);
   int i;
   for (i = 0; i < N; i++) {
     xs[i] = a + h * i;
--- tests/integrate.c
+++ tests/integrate.c
@@ -9,24 +9,26 @@
   X(f2, results_f2)                                                            \
   X(f3, results_f3)                                                            \
   X(f4, results_f4)                                                            \
-  X(f5, results_f5)
+  X(f5, results_f5)                                                            \
+  X(f6, results_f6)
 
-#define N 25000
-#define EPS 0.1f
+#define N 250000
+#define EPS 0.0001
 
-float f1(float x) { return x; }
-float f2(float x) { return x * x; }
-float f3(float x) { return sinf(x); }
-float f4(float x) { return expf(x); }
-float f5(float x) { return sinf(x) * expf(x); }
+double f1(double x) { return x; }
+double f2(double x) { return x * x; }
+double f3(double x) { return sin(x); }
+double f4(double x) { return exp(x); }
+double f5(double x) { return sin(x) * exp(x); }
+double f6(double x) { return x * x + x - 2; }
 
-float limits_a[] = {
+double limits_a[] = {
     0,
     -5,
     0,
     -5,
 };
-float limits_b[] = {
+double limits_b[] = {
     1,
     5,
     10,
@@ -37,27 +39,38 @@ float results_f1[] = {
     0.5,
     0.0,
     50.0,
+    -8.0,
 };
 float results_f2[] = {
     0.33333333333333337,
     83.33333333333334,
     333.33333333333326,
+    32.66666666666667,
 };
 float results_f3[] = {
     0.45969769413186023,
     0.0,
     1.8390715290764525,
+    1.2736546820636718,
 };
 float results_f4[] = {
     1.7182818284590453,
     148.40642115557753,
     22025.465794806725,
+    0.043049121368778476,
 };
-
 float results_f5[] = {
     0.9093306736314788,
     -92.2103659428046,
     3249.9589719705905,
+    0.01885649622752392,
+};
+
+float results_f6[] = {
+    -1.1666666666666667,
+    63.33333333333334,
+    363.33333333333337,
+    20.666666666666668,
 };
 
 size_t num_limits = sizeof(limits_a) / sizeof(limits_a[0]);
@@ -65,13 +78,13 @@ size_t num_limits = sizeof(limits_a) / sizeof(limits_a[0]);
 int main() {
 
   ct_verbose = 2;
-  float I_est_sq, I_est_tr, I_true;
+  double I_est_sq, I_est_tr, I_true;
   int i;
 
   for (i = 0; i < num_limits; i++) {
 #define X(f, results_f)                                                        \
-  I_est_sq = squares(limits_a[i], limits_b[i], N, f);                          \
-  I_est_tr = trapecia(limits_a[i], limits_b[i], N, f);                         \
+  I_est_sq = squaresd(limits_a[i], limits_b[i], N, f);                         \
+  I_est_tr = trapeciad(limits_a[i], limits_b[i], N, f);                        \
   I_true = results_f[i];                                                       \
   ct_assert_float(I_est_sq, I_true, EPS);                                      \
   ct_assert_float(I_est_tr, I_true, EPS);
