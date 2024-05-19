8eb923c8b657dd817e3fd2a97607f2d7bf7c7c25
Basyrov Rustam <bronit10@morozko.ru>
2024-05-19 08:49:47 +0300

    implement benchmarking

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -19,6 +19,7 @@ int main() {
   FILE *result_file = fopen(RESULT_PATH, "w");
   int i;
   float result1, result2;
+  float result3, result4;
   float a = 0.0f;
   float b = 10.0f;
   float true_I = (b * b * b) / 3.0f - (a * a * a) / 3.0f;
@@ -27,8 +28,10 @@ int main() {
   for (i = 2; i < MAX_N; i++) {
     result1 = old_squares(a, b, i, fun);
     result2 = new_squares(a, b, i, fun);
-    fprintf(STREAM, "%d,%f,%f\n", i, fabs(result1 - true_I),
-            fabs(result2 - true_I));
+    result3 = old_trapecia(a, b, i, fun);
+    result4 = new_trapecia(a, b, i, fun);
+    fprintf(STREAM, "%d,%f,%f\n", i, fabs(result3 - true_I),
+            fabs(result4 - true_I));
   }
 
   fclose(result_file);
