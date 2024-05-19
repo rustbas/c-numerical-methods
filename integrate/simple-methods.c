98524d364192ea1534e137f6ba0cebdbe5cf1d8d
Basyrov Rustam <bronit10@morozko.ru>
2024-05-19 08:42:07 +0300

    merge

---
+++ integrate/simple-methods.c
@@ -0,0 +1,75 @@
+#ifndef _INTEGRATE_SIMPLE_METHODS
+#define _INTEGRATE_SIMPLE_METHODS
+
+// Dirty hack
+#ifndef INTEGRATE_SM_IMPLEMENTATION
+#define INTEGRATE_SM_IMPLEMENTATION
+#define _INTEGRATE_SM_IMPLEMENTATION
+#endif
+
+#ifndef MATH_H
+#define MATH_H
+#include <math.h>
+#endif
+
+#ifndef STDLIB_H
+#define STDLIB_H
+#include <stdlib.h>
+#endif
+
+#include <stdio.h>
+
+float old_squares(float a, float b, unsigned N, float (*f)(float));
+float trapecia(float a, float b, unsigned N, float (*f)(float));
+#endif
+
+// ----------------------------------------
+// ----------------------------------------
+// ----------------------------------------
+
+#ifdef INTEGRATE_SM_IMPLEMENTATION
+// TODO: Fix error accumulation
+float old_squares(float a, float b, unsigned N, float (*f)(float)) {
+  float h = (b - a) / N;
+  float result = 0.0;
+
+  while (a < b) {
+    result += h * f(a + h / 2);
+    a += h;
+  }
+
+  return result;
+}
+
+float new_squares(float a, float b, unsigned N, float (*f)(float)) {
+  float h = (b - a) / (N - 1);
+  float result = 0.0;
+  float *xs = (float *)malloc(sizeof(float) * N);
+  int i;
+  for (i = 0; i < N; i++) {
+    xs[i] = a + h * i;
+  }
+
+  for (i = 0; i < N - 1; i++) {
+    // result += (xs[i] - xs[i - 1]) * f((xs[i] + xs[i - 1]) / 2);
+    result += h * f((xs[i] + xs[i + 1]) / 2);
+  }
+
+  free(xs);
+
+  return result;
+}
+
+float trapecia(float a, float b, unsigned N, float (*f)(float)) {
+  float h = (b - a) / N;
+  float result = 0.0;
+
+  while (a < b) {
+    result += h * (f(a) + f(a + h)) / 2;
+    a += h;
+  }
+
+  return result;
+}
+
+#endif
