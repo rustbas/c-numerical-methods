edc753673b1fc344e5d7158e9bc728ef0f3fb0fd
Basyrov Rustam <bronit10@morozko.ru>
2024-05-19 09:01:19 +0300

    Add some task

--- integrate/simple-methods.c
+++ integrate/simple-methods.c
@@ -26,6 +26,9 @@ float trapecia(float a, float b, unsigned N, float (*f)(float));
 // ----------------------------------------
 
 #ifdef INTEGRATE_SM_IMPLEMENTATION
+
+// TODO: Create `double` versions
+
 float squares(float a, float b, unsigned N, float (*f)(float)) {
   float h = (b - a) / (N - 1);
   float result = 0.0;
